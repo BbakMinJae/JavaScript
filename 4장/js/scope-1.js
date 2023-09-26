52b7c3f60ad10178613c3fde35a7d6bb3fadb8cc
BbakMinJae <129822546+BbakMinJae@users.noreply.github.com>
2023-09-26 13:18:56 +0900

    Delete 4장 directory

--- 4장/js/scope-1.js
+++
@@ -1,7 +0,0 @@
-function sum (num1, num2) {
-    var result = num1 + num2;   //지역 변수
-}
-
-sum(10, 20);
-console.log(result);    //선언된 변수 x
-                        //오류 result is not define.
--- 4장/js/scope-2.js
+++
@@ -1,7 +0,0 @@
-var hi = "hello";   //전역 변수
-
-function greeting() {
-    console.log(hi) //hi의 내용이 출력되나?
-}
-
-greeting(); //함수 호출
